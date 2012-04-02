987fe61fd73d1faa89fcc8b6b3c159e9ab55c904
Brad Campbell <bradjc@ubuntu.(none)>
2012-04-02 00:22:03 -0400

    added a lot of printing functionality

--- jsIP.js
+++ jsIP.js
@@ -1,131 +1,222 @@
+var compactIPv4 = function (bytes) {
+	return [(bytes[0]<<8) + bytes[1], (bytes[2]<<8) + bytes[3]];
+}
 
-var parseIPv4 = function (addr) {
-	bytes = addr.split('.');
+var parseIPv4 = function (addr_str) {
+	bytes = addr_str.split('.');
 	if (bytes.length > 4) throw "IPv4 address too long";
 	filler = [];
 	for (i=0; i<8; i++) { filler.push(0); }
 	bytes = new Array().concat(filler.slice(0, (4 - bytes.length)), bytes);
-
 	for (i in bytes) {
 		bytes[i] = parseInt(bytes[i]);
+		if (bytes[i] > 255) throw 'IPv4 Error: section too large';
 	}
-
 	return bytes;
 }
+
+var parseIPv6 = function (addr_str) {
+	// IPv6 address
+	var index    = 0
+	var items    = []
+	var fill_pos = undefined
+
+	while (index < addr_str.length) {
+		text = addr_str.substring(index);
+		if (text.substring(0, 2) == '::') {
+			if (fill_pos != undefined) throw "IPv6 Error: can't have two '::'";
+			fill_pos = items.length;
+			index    += 2;
+			continue;
+		}
+		pos = text.indexOf(':');
+		if (pos == 0) throw "IPv6 Error: missing ':'";
+		if (pos != -1) {
+			items.push(text.substring(0, pos));
+			if (text.substring(pos, pos+2) == '::') {
+				index += pos;
+			} else {
+				index += pos+1;
+			}
+
+			if (index == addr_str.length) throw "IPv6 Error: Invalid";
+		} else {
+			items.push(text);
+			break
+		}
+	}
+
+	if (items.length > 0 && items[items.length-1].indexOf('.') > -1) {
+		// IPv4 address
+		if (fill_pos != undefined && !(fill_pos <= items.length-1)) {
+			throw "IPv6 Error: invalid IPv4 representation";
+		}
+		ipv4  = parseIPv4(items[items.length-1])
+		items = new Array().concat(items.slice(0,items.length-1),
+		                           [((ipv4[0]<<8) + ipv4[1]).toString(16)],
+		                           [((ipv4[2]<<8) + ipv4[3]).toString(16)]);
+	}
+
+	if (fill_pos != undefined) {
+		diff = 8 - items.length;
+		if (diff <= 0) throw "IPv6 Error: found '::' but no sections skipped";
+		filler = [];
+		for (i=0; i<8; i++) { filler.push(0); }
+		items = new Array().concat(items.slice(0, fill_pos),
+		                           filler.slice(0, diff),
+		                           items.slice(fill_pos, items.length));
+	}
+
+	if (items.length != 8) throw "IPv6 Error: too short";
+
+	for (i in items) {
+		items[i] = parseInt(items[i], 16);
+	}
+
+	return items;
+}
+
+var printIPv4 = function (ip) {
+	bytes = []
+	bytes.push(ip[6]>>8);
+	bytes.push(ip[6]&0xff);
+	bytes.push(ip[7]>>8);
+	bytes.push(ip[7]&0xff);
+	return bytes.join('.');
+}
+
+var printFullIPv6 = function (ip) {
+	out = []
+	for (i in ip) {
+		val = ip[i].toString(16);
+		while (val.length < 4) {
+			val = '0' + val;
+		}
+		out.push(val);
+	}
+	return out.join(':');
+}
+
+var printNormalIPv6 = function (ip) {
+	out = []
+	for (i in ip) {
+		out.push(ip[i].toString(16));
+	}
+	return out.join(':');
+}
+
+var printCompressedIPv6 = function (ip, start, skip) {
+	out = []
+	end = start + skip;
+	for (i in ip) {
+		if (i < start || i >= end) {
+			out.push(ip[i].toString(16));
+		} else if (i == start) {
+			out.push(':');
+		}
+	}
+	return out.join(':').replace(':::', '::');
+}
+
+
+
 
 
 IP = function (addr, version) {
 //exports.IP = function (addr, version) {
+	if (!(this instanceof IP)) return new IP(addr, version);
 
-//	if (version == "undefined") {
-//		version = 0;
-//	}
+
+	var self = this;
+
+	if (version == "undefined") {
+		self.version = 0;
+	}
 
 
 	// represent IP address as 8 16 bit ints (which are floats in reality)
-	var ip = [];
-	for (i=0; i<8; i++) { ip.push(0); }
+	self.ip = [];
+	for (i=0; i<8; i++) { self.ip.push(0); }
 
 	if (typeof(addr) == 'string') {
 
 		if (addr.substring(0, 2) == '0x') {
 			// Hex value as string
 			var rest = addr.substring(2);
-			for (i=0; i<Math.min(rest.length, 8); i+=4) {
+			for (i=0; i<Math.min(rest.length, 32); i+=4) {
 				end = rest.length-i;
 				start = Math.max(rest.length-4-i, 0);
 				substr = rest.substring(start, end);
-				ip[7-(i/4)] = parseInt(substr, 16);
+				self.ip[7-(i/4)] = parseInt(substr, 16);
 			}
+			self.version = 6;
 
 		} else if (addr.indexOf(':') > -1) {
-			// IPv6 address
-			var index = 0
-			var fill_pos
-			var items = []
-
-			while (index < addr.length) {
-				text = addr.substring(index);
-				if (text.substring(0, 2) == '::') {
-					if (fill_pos != undefined) {
-						//error
-						console.log('err');
-					}
-					fill_pos = items.length;
-					index += 2;
-					continue;
-				}
-				pos = text.indexOf(':');
-				if (pos == 0) {
-					console.log('err 2');
-				}
-				if (pos != -1) {
-					items.push(text.substring(0, pos));
-					if (text.substring(pos, pos+2) == '::') {
-						index += pos;
-					} else {
-						index += pos+1;
-					}
-
-					if (index == addr.length) {
-						console.log('err 3');
-					}
-				} else {
-					items.push(text);
-					break
-				}
-			}
-			console.log(items);
-
-			if (items.length > 0 && items[items.length-1].indexOf('.') > -1) {
-				// IPv4 address
-				if (fill_pos != undefined && !(fill_pos <= items.length-1)) {
-					console.log('err 4');
-				}
-				// FINISH PARSING IPV4 address
-				console.log('ipv4: ' + items);
-				ipv4 = parseIPv4(items[items.length-1])
-				items = new Array().concat(items.slice(0,items.length-1),
-				                           [((ipv4[0]<<8) + ipv4[1]).toString(16)],
-				                           [((ipv4[2]<<8) + ipv4[3]).toString(16)]);
-			}
-
-			if (fill_pos != undefined) {
-				diff = 8 - items.length;
-				if (diff <= 0) {
-					console.log('err 5');
-				}
-				filler = [];
-				for (i=0; i<8; i++) { filler.push(0); }
-				items = new Array().concat(items.slice(0, fill_pos),
-				                           filler.slice(0, diff),
-				                           items.slice(fill_pos, items.length));
-				console.log('this items');
-				console.log(items);
-
-
-			}
-
-			if (items.length != 8) {
-				console.log('err 6');
-			}
-
-			for (i in items) {
-				items[i] = parseInt(items[i], 16);
-			}
-			ip = items;
-
-			console.log(items);
+			self.ip = parseIPv6(addr);
+			self.version = 6;
+		} else if (addr.indexOf('.') > -1) {
+			v4 = compactIPv4(parseIPv4(addr));
+			self.ip[6] = v4[0];
+			self.ip[7] = v4[1];
+			self.version = 4;
 		}
 
 	}
-	console.log('ip: ' + ip);
+//	console.log('ip: ' + ip);
+
+	self.str = function () {
+		if (self.version == 4) {
+			return printIPv4(self.ip);
+
+		} else if (self.version == 6) {
+
+			if (self.ip[0] == 0 &&
+			    self.ip[1] == 0 &&
+			    self.ip[2] == 0 &&
+			    self.ip[3] == 0 &&
+			    self.ip[4] == 0 &&
+			    self.ip[5] == 0xffff) {
+				// ipv4 address as ipv6 address
+				return "::ffff:" + printIPv4(self.ip);
+
+			} else {
+				long_run       = 0;
+				long_run_start = -1;
+				run            = 0;
+				run_start      = -1;
+				for (i=0; i<9; i++) {
+					if (i<8 && self.ip[i] == 0) {
+						run++;
+						if (run_start == -1) {
+							run_start = i;
+						}
+					} else {
+						if (run > long_run) {
+							long_run       = run;
+							long_run_start = run_start;
+						}
+						run       = 0;
+						run_start = -1;
+					}
+				}
+				return printCompressedIPv6(self.ip, long_run_start, long_run);
+
+			}
+
+
+		}
+	}
 
 }
-
-IP('0xfffabcde');
-IP('::ffff:8a:9c');
-IP('67::ffff:8a:9c');
-IP('::ffff:192.168.1.1');
+/*
+ip = IP('0xfffabcde');
+//console.log(ip);
+ip.str();
+IP('::ffff:8a:9c');*/
+console.log(IP('67::ffff:8a:9c').str());
+console.log(IP('67:ffff:8a:9c::5').str());
+console.log(IP('0xffff8dd46ef5').str());
+console.log(IP('::ffff:192.168.1.1').str());
+console.log(IP('192.255.9.10').str());
 
 
